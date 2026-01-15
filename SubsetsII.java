22f238a7dda0b7e9168b7e2ba8022ee53ab92fe7
TengKimhan <tengkimhan@gmail.com>
2026-01-15 15:14:29 +0700

    Solve problem 90. Subsets II

---
+++ SubsetsII.java
@@ -0,0 +1,45 @@
+// 90. Subsets II
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+    Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+    The solution set must not contain duplicate subsets. Return the solution in any order.
+
+    Example 1:
+    Input: nums = [1,2,2]
+    Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
+    Example 2:
+    Input: nums = [0]
+    Output: [[],[0]]
+ */
+public class SubsetsII {
+    public static void main(String[] args) {
+        List<List<Integer>> result = subsetsWithDup(new int[]{1, 2, 2});
+        System.out.println(result);
+    }
+
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null) return res;
+        Arrays.sort(nums);
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    public static void backtrack(int[] nums, int start, List<Integer> curr, List<List<Integer>> res) {
+        res.add(new ArrayList<>(curr));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            curr.add(nums[i]);
+            backtrack(nums, i + 1, curr, res);
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
+
+
