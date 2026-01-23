fc3b7e8b28c600dc5ec3055c4022c37f20ea4c1b
TengKimhan <tengkimhan@gmail.com>
2026-01-23 09:46:46 +0700

    Solve problem 3142. Check if Grid Satisfies Conditions

---
+++ CheckIfGridSatisfiesConditions.java
@@ -0,0 +1,27 @@
+// 3142. Check if Grid Satisfies Conditions
+
+/*
+    You are given a 2D matrix grid of size m x n. You need to check if each cell grid[i][j] is:
+        1. Equal to the cell below it, i.e. grid[i][j] == grid[i + 1][j] (if it exists).
+        2. Different from the cell to its right, i.e. grid[i][j] != grid[i][j + 1] (if it exists).
+
+    Return true if all the cells satisfy these conditions, otherwise, return false.
+*/
+
+public class CheckIfGridSatisfiesConditions {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean satisfiesConditions(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (i+1 < row && grid[i][j] != grid[i+1][j]) return false;
+                if (j+1 < col && grid[i][j] == grid[i][j+1]) return false;
+            }
+        }
+        return true;
+    }
+}
