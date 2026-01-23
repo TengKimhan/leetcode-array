399bfc23082fb0bfbb8cb0f6c0afe469d547353a
TengKimhan <tengkimhan@gmail.com>
2026-01-23 10:28:45 +0700

    Solve problem 2144. Minimum Cost of Buying Candies With Discount

---
+++ MinimumCostOfBuyingCandiesWithDiscount.java
@@ -0,0 +1,25 @@
+// 2144. Minimum Cost of Buying Candies With Discount
+
+import java.util.Arrays;
+
+public class MinimumCostOfBuyingCandiesWithDiscount {
+    public static void main(String[] args) {
+
+    }
+
+    public int minimumCost(int[] cost) {
+        int sum=0;
+        int len=cost.length;
+        Arrays.sort(cost);
+
+        int val = 1;
+        for (int i=len-1; i>=0; i--) {
+            if (val%3==0) val=1;
+            else {
+                sum+=cost[i];
+                val++;
+            }
+        }
+        return sum;
+    }
+}
